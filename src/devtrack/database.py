9f0648b6735720e400b9ce18d6ffe5ca7157167e
pstrings <masuya444@gmail.com>
2026-09-14 00:51:13 +0530

    database: added typed annotation for return type

--- src/devtrack/database.py
+++ src/devtrack/database.py
@@ -2,6 +2,7 @@ import os
 
 import psycopg
 from dotenv import load_dotenv
+from psycopg.rows import dict_row
 
 load_dotenv()
 
@@ -23,58 +24,47 @@ def create_tables():
                 )
             """
         )
-        connection.commit()
 
 
-def select_tasks():
+def select_tasks() -> list[tuple[int, str, str]]:
     with get_connection() as connection, connection.cursor() as cursor:
         cursor.execute("SELECT id, title, status FROM tasks ORDER BY id")
-        rows = cursor.fetchall()
 
+        rows = cursor.fetchall()
         return rows
 
 
-def select_task(task_id: int):
+def select_task(task_id: int) -> tuple[int, str, str] | None:
     with get_connection() as connection, connection.cursor() as cursor:
         cursor.execute(
             "SELECT id, title, status FROM tasks WHERE id=%s", (task_id,))
 
         row = cursor.fetchone()
-
         return row
 
 
-def insert_task(title: str, status: str):
+def insert_task(title: str, status: str) -> tuple[int, str, str] | None:
     with get_connection() as connection, connection.cursor() as cursor:
         cursor.execute(
             "INSERT INTO tasks (title, status) VALUES (%s, %s) RETURNING id, title, status", (title, status))
 
         row = cursor.fetchone()
-
-        connection.commit()
-
         return row
 
 
-def update_task(task_id: int, title: str, status: str):
+def update_task(task_id: int, title: str, status: str) -> tuple[int, str, str] | None:
     with get_connection() as connection, connection.cursor() as cursor:
         cursor.execute(
             "UPDATE tasks SET title=%s, status=%s WHERE id=%s RETURNING id, title, status", (title, status, task_id))
 
         row = cursor.fetchone()
-
-        connection.commit()
-
         return row
 
 
-def delete_task(task_id: int):
+def delete_task(task_id: int) -> tuple[int, str, str] | None:
     with get_connection() as connection, connection.cursor() as cursor:
         cursor.execute(
             "DELETE FROM tasks WHERE id=%s RETURNING id, title, status", (task_id,))
 
         row = cursor.fetchone()
-
-        connection.commit()
-
         return row
